bca9a74658ef767665c0e4c3d04df686a30416eb
Toni Melisma <59022391+tonimelisma@users.noreply.github.com>
2023-11-23 23:52:19 -0800

    refactor auth to SDK

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -9,59 +8,100 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"time"
 
-	cv "github.com/nirasan/go-oauth-pkce-code-verifier"
 	"github.com/tonimelisma/onedrive-sdk-go"
-	"golang.org/x/oauth2"
 )
-
-// UTILITIES
-
-func debug(config Configuration, message string) {
-	if config.Debug {
-		log.Println(message)
-	}
-}
-
-// CONFIGURATION HANDLING
 
 const configDir = ".onedrive-client"
 const configFile = "config.json"
 
 type Configuration struct {
-	Token oauth2.Token `json:"token"`
-	Debug bool         `json:"debug"`
+	Token onedrive.OAuthToken `json:"token"`
+	Debug bool                `json:"debug"`
 }
 
-func loadConfiguration() (thisConfig Configuration, err error) {
+func main() {
+	config, err := loadConfiguration()
+	if err != nil {
+		handleConfigurationError(err, &config)
+	}
+
+	config.Debug = true
+	client, err := initializeOneDriveClient(&config)
+	if err != nil {
+		log.Fatalf("Error during OneDrive client initialization: %v\n", err)
+	}
+
+	fmt.Println("Getting drives...")
+	err = onedrive.GetMyDrives(client)
+	if err != nil {
+		log.Fatalf("Error getting drives: %v\n", err)
+	}
+}
+
+func initializeOneDriveClient(config *Configuration) (*http.Client, error) {
+	if config.Token.AccessToken == "" {
+		authURL, codeVerifier, err := onedrive.StartAuthentication()
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Println("Visit the following URL in your browser and authorize the app:", authURL)
+		fmt.Print("Enter the authorization code: ")
+
+		var redirectURL string
+		fmt.Scan(&redirectURL)
+
+		parsedUrl, err := url.Parse(redirectURL)
+		if err != nil {
+			return nil, fmt.Errorf("parsing redirect URL: %v", err)
+		}
+
+		code := parsedUrl.Query().Get("code")
+		if code == "" {
+			return nil, fmt.Errorf("authorization code not found in the URL")
+		}
+
+		token, err := onedrive.CompleteAuthentication(code, codeVerifier)
+		if err != nil {
+			return nil, err
+		}
+
+		config.Token = *token
+		err = saveConfiguration(*config)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return onedrive.NewClient(config.Token), nil
+}
+
+func loadConfiguration() (Configuration, error) {
+	var config Configuration
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		return thisConfig, fmt.Errorf("getting home directory: %v", err)
+		return config, fmt.Errorf("getting home directory: %v", err)
 	}
 
 	configPath := filepath.Join(homeDir, configDir, configFile)
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		return thisConfig, err
-	}
-
 	fileHandle, err := os.ReadFile(configPath)
 	if err != nil {
-		return thisConfig, fmt.Errorf("reading file: %v", err)
+		return config, err
 	}
 
-	err = json.Unmarshal([]byte(fileHandle), &thisConfig)
+	err = json.Unmarshal(fileHandle, &config)
 	if err != nil {
-		return thisConfig, fmt.Errorf("unmarshalling json: %v", err)
+		return config, fmt.Errorf("unmarshalling json: %v", err)
 	}
 
-	return thisConfig, nil
+	return config, nil
 }
 
-func saveConfiguration(thisConfig Configuration) (err error) {
-	jsonData, err := json.MarshalIndent(thisConfig, "", "")
+func saveConfiguration(config Configuration) error {
+	jsonData, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
-		return fmt.Errorf("marshalling json: %v", err)
+		return fmt.Errorf("marshalling config to JSON: %v", err)
 	}
 
 	homeDir, err := os.UserHomeDir()
@@ -71,177 +111,24 @@ func saveConfiguration(thisConfig Configuration) (err error) {
 
 	configDirPath := filepath.Join(homeDir, configDir)
 	if _, err := os.Stat(configDirPath); os.IsNotExist(err) {
-		err = os.Mkdir(configDirPath, 0700)
-		if err != nil {
-			return fmt.Errorf("creating configuration directory: %v", err)
+		if err := os.Mkdir(configDirPath, 0700); err != nil {
+			return fmt.Errorf("creating config directory: %v", err)
 		}
 	}
 
 	configFilePath := filepath.Join(configDirPath, configFile)
-	err = os.WriteFile(configFilePath, jsonData, 0600)
-	if err != nil {
-		return fmt.Errorf("writing configuration: %v", err)
+	if err := os.WriteFile(configFilePath, jsonData, 0600); err != nil {
+		return fmt.Errorf("writing configuration file: %v", err)
 	}
 
 	return nil
 }
 
-// MAIN PROGRAM
-
-func main() {
-	// Load configuration, and initialize OAuth2 tokens
-	config, err := loadConfiguration()
-	config.Debug = true
+func handleConfigurationError(err error, config *Configuration) {
 	if os.IsNotExist(err) {
-		fmt.Println("No configuration file found.")
-	} else if err != nil {
-		fmt.Fprintf(os.Stderr, "couldn't load configuration: %v\n", err)
-		os.Exit(1)
-	}
-
-	var client *http.Client
-
-	if config.Token.AccessToken == "" {
-		debug(config, "No access token found.")
-		client, err = authenticate(&config)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "couldn't authenticate: %v\n", err)
-			os.Exit(1)
-		}
+		fmt.Println("No configuration file found. Proceeding with authentication.")
+		config.Token = onedrive.OAuthToken{} // Reset the token
 	} else {
-		debug(config, "Access token found.")
-		client, err = validateToken(&config)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Couldn't validate token: %v\n", err)
-			os.Exit(1)
-		}
+		log.Fatalf("Couldn't load configuration: %v\n", err)
 	}
-
-	// OAuth2 tokens ready
-	fmt.Println("Getting drives...")
-
-	onedrive.GetMyDrives(client)
-}
-
-// OAUTH2 STUFF
-
-func getOauth2Config() (context.Context, *oauth2.Config) {
-	return context.Background(), &oauth2.Config{
-		ClientID: "71ae7ad2-0207-4618-90d3-d21db38f9f7a",
-		Scopes:   []string{"offline_access", "files.readwrite.all"},
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
-			TokenURL: "https://login.microsoftonline.com/common/oauth2/v2.0/token",
-		},
-	}
-}
-
-func authenticate(config *Configuration) (*http.Client, error) {
-	ctx, oauthConfig := getOauth2Config()
-
-	codeVerifier, err := cv.CreateCodeVerifier()
-	if err != nil {
-		return nil, fmt.Errorf("creating code verifier: %v", err)
-	}
-	codeChallenge := codeVerifier.CodeChallengeS256()
-
-	codeChallengeAuthParam := oauth2.SetAuthURLParam("code_challenge", codeChallenge)
-	codeChallengeMethodAuthParam := oauth2.SetAuthURLParam("code_challenge_method", "S256")
-	fmt.Println("Visit the following URL in your browser and authorize the app.")
-	fmt.Println(oauthConfig.AuthCodeURL("local", codeChallengeAuthParam, codeChallengeMethodAuthParam))
-	fmt.Println("")
-	fmt.Println("You'll be redirected to an empty page. Copy-paste its URL here:")
-
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		return nil, fmt.Errorf("scanning [%v]: %v", code, err)
-	}
-	fmt.Println("")
-
-	// Parse oauth callback URL to ensure there's no error
-	parsedUrl, err := url.Parse(code)
-	if err != nil {
-		return nil, fmt.Errorf("parsing url [%v]: %v", code, err)
-	}
-
-	// Parse oauth callback URL's query parameters
-	parsedQuery, err := url.ParseQuery(parsedUrl.RawQuery)
-	if err != nil {
-		return nil, fmt.Errorf("parsing query [%v]: %v", code, err)
-	}
-
-	// If callback returned an error, bring it up
-	if parsedQuery.Has("error") {
-		if parsedQuery.Has("error_description") {
-			return nil, fmt.Errorf("oauth authorization failed: %v: %v", parsedQuery.Get("error"), parsedQuery.Get("error_description"))
-		} else {
-			return nil, fmt.Errorf("oauth authorization failed: %v", parsedQuery.Get("error"))
-		}
-	}
-
-	if !parsedQuery.Has("code") {
-		return nil, fmt.Errorf("couldn't parse code in callback: %v", parsedQuery.Encode())
-	}
-
-	codeVerifierAuthParam := oauth2.SetAuthURLParam("code_verifier", codeVerifier.String())
-	token, err := oauthConfig.Exchange(ctx, parsedQuery.Get("code"), codeVerifierAuthParam)
-	if err != nil {
-		return nil, err
-	}
-
-	err = saveToken(*token, config)
-	if err != nil {
-		return nil, err
-	}
-
-	client := oauthConfig.Client(ctx, token)
-	return client, nil
-}
-
-// saveToken copies the token into the configuration and saves it to cache
-func saveToken(token oauth2.Token, config *Configuration) error {
-	debug(*config, "Old token: "+config.Token.AccessToken)
-	debug(*config, "New token: "+token.AccessToken)
-	config.Token = token
-	err := saveConfiguration(*config)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// checkAndSaveToken() takes an oauth2 token and refreshes and saves it if its expired
-func checkAndSaveToken(token *oauth2.Token, config *Configuration) error {
-	ctx, oauthConfig := getOauth2Config()
-	if token.Expiry.Before(time.Now()) {
-		debug(*config, "Token expired, refreshing...")
-		tokenSource := oauthConfig.TokenSource(ctx, token)
-		newToken, err := tokenSource.Token()
-		if err != nil {
-			return fmt.Errorf("couldn't refresh token: %v", err)
-		}
-		if newToken.AccessToken != token.AccessToken {
-			debug(*config, "Token has changed, saving new token...")
-			saveToken(*newToken, config)
-			token = newToken
-		}
-	} else {
-		debug(*config, "Token not yet expired: "+token.Expiry.String())
-	}
-	return nil
-}
-
-// validateToken restores a token from the configuration and refreshes it
-func validateToken(config *Configuration) (*http.Client, error) {
-	ctx, oauthConfig := getOauth2Config()
-
-	debug(*config, "Validating token...")
-
-	err := checkAndSaveToken(&config.Token, config)
-	if err != nil {
-		return nil, err
-	}
-
-	client := oauthConfig.Client(ctx, &config.Token)
-	return client, nil
 }
